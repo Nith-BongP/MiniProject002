673d7fb999efeabced44f1bfd1d96d81fb37b7bb
BCSotheany <bchansotheany@gmail.com>
2026-06-11 16:30:53 +0700

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,3 +1,29 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
 public class Controller {
-    
+
+    @FXML
+    private ChoiceBox<?> Choicebox1;
+
+    @FXML
+    private ChoiceBox<?> Choicebox2;
+
+    @FXML
+    private ChoiceBox<?> Choicebox3;
+
+    @FXML
+    private Button button_submit;
+
+    @FXML
+    private Label minip;
+
+    @FXML
+    private Label result;
+
+    @FXML
+    private Label timmer;
+
 }
